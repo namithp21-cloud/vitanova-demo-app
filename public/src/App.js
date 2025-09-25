7745d6e3c813d1eb68b845ad4e9e45d406f0eb7f
namithp21-cloud <namithp21@gmail.com>
2025-09-25 09:40:55 +0530

    Create App.js

---
+++ public/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+export default function VitanovaApp() {
+  return (
+    <div>
+      <h1>Vitanova Demo App</h1>
+      <p>Your main code goes here!</p>
+    </div>
+  );
+}
