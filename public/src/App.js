5b0584cdf2dc1255ab5005b28bddcc9f562cdf0a
namithp21-cloud <namithp21@gmail.com>
2025-09-25 09:43:39 +0530

    Delete public/src directory

--- public/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export default function VitanovaApp() {
-  return (
-    <div>
-      <h1>Vitanova Demo App</h1>
-      <p>Your main code goes here!</p>
-    </div>
-  );
-}
